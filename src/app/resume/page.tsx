febb6d2a4cecc08b43bf8f22b961389f64e2f884
Lukas Sorensen <lukasasorensen@gmail.com>
2024-07-05 22:43:08 -0500

    format resume desc

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -7,13 +7,15 @@ export default function Resume() {
 
   return (
     <main className={`flex min-h-screen flex-col items-center justify-around ${twColorClasses.BG_PRIMARY} p-24`}>
-      <h1 className={`mb-10 text-center text-4xl font-bold ${twColorClasses.TEXT_SECONDARY}`}>My Resume</h1>
-      <p className="mb-3 px-24">
-        Lukas Sorensen is an experienced Full Stack Web Developer with over 10 years of expertise in writing SOLID,
-        maintainable, and extendable code. Passionate about continuous learning, communicative and adept at
-        problem-solving.
-      </p>
-      <ResumeTimeline />
+      <div className="max-w-screen-lg flex flex-col items-center">
+        <h1 className={`mb-10 text-center text-4xl font-bold ${twColorClasses.TEXT_SECONDARY}`}>My Resume</h1>
+        <p className="mb-3 max-w-screen-md">
+          Lukas Sorensen is an experienced Full Stack Web Developer with over 10 years of expertise in writing SOLID,
+          maintainable, and extendable code. Passionate about continuous learning, communicative and adept at
+          problem-solving.
+        </p>
+        <ResumeTimeline />
+      </div>
     </main>
   );
 }
