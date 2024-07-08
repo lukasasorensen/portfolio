5a5239b3b25f842e2b646e218f19e93e03b6377f
Lukas Sorensen <lsorensen@sportsdigita.com>
2024-07-07 21:37:45 -0500

    redo home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,19 +8,15 @@ export default function Home() {
   const { twColorClasses } = useThemeContext();
 
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-around ${twColorClasses.BG_PRIMARY} p-24`}
-    >
-      <h1
-        className={`mb-10 text-center text-4xl font-bold ${twColorClasses.TEXT_SECONDARY}`}
-      >
-        lukasasorensen.com
-      </h1>
-      <ThemedButton
-        text="Projects"
-        color="primary"
-        onClick={() => router.push("/projects")}
-      />
+    <main className={`flex min-h-screen flex-col items-center justify-around ${twColorClasses.BG_PRIMARY} p-24`}>
+      <div className="text-center">
+        <h1 className={`mb-5 text-center text-7xl font-bold ${twColorClasses.TEXT_SECONDARY}`}>Lukas A Sorensen</h1>
+        <h2 className={`text-2xl uppercase ${twColorClasses.TEXT_TERTIARY}`}>Full Stack Engineer</h2>
+      </div>
+      <div className="flex justify-evenly w-full max-w-screen-sm">
+        <ThemedButton color="primary" className="text-2xl" text="Resume" color="primary" onClick={() => router.push("/resume")} />
+        <ThemedButton color="secondary" className="text-2xl" text="Contact" color="primary" onClick={() => router.push("/contact")} />
+      </div>
     </main>
   );
 }
