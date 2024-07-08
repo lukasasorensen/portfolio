dff4d90a5f3a1f263af58647bf7df6e650ad2e61
Lukas Sorensen <lsorensen@sportsdigita.com>
2024-07-07 21:21:42 -0500

    add another article, refactor example article data to array

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -9,24 +9,30 @@ import Articles, { IArticle } from "@/example-data/Articles";
 export default function Blog() {
   const { twColorClasses } = useThemeContext();
   const router = useRouter();
-  
+
   const getArticle = (article: IArticle): IProject => {
     return {
-        id: article.id,
-        title: article.title,
-        createdDate: article.createdDate,
-        description: article.articleText,
-        imageSrc: article.imageSrc,
-        shortDescription: article.articleDescription ?? ''
-    }
-  }
+      id: article.id,
+      title: article.title,
+      createdDate: article.createdDate,
+      description: article.articleText,
+      imageSrc: article.imageSrc,
+      shortDescription: article.articleDescription ?? "",
+    };
+  };
 
   return (
     <main className={`flex min-h-screen flex-col items-center ${twColorClasses.BG_PRIMARY} px-24 py-14`}>
       <h1 className={`mb-10 text-center text-4xl font-bold ${twColorClasses.TEXT_SECONDARY}`}>Blog</h1>
       <div className="max-w-screen-lg space-y-6">
         <div className="grid grid-cols-1 gap-x-4 gap-y-8 md:grid-cols-2">
-          <ProjectListing onClick={() => router.push(`/blog/${Articles.SYNC_DOTFILES.id}`)} project={getArticle(Articles.SYNC_DOTFILES)} />
+          {Articles.map((article) => (
+            <ProjectListing
+              key={"article-listing-" + article.id}
+              onClick={() => router.push(`/blog/${article.id}`)}
+              project={getArticle(article)}
+            />
+          ))}
         </div>
       </div>
     </main>
