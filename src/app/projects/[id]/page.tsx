3c4d3f091faa6bb6fd8b6803275cd5835c6128c3
Lukas Sorensen <lsorensen@sportsdigita.com>
2024-07-07 19:10:42 -0500

    format project article page

--- src/app/projects/[id]/page.tsx
+++ src/app/projects/[id]/page.tsx
@@ -24,17 +24,17 @@ export default function ProjectDetail({ params }: { params: { id: string } }) {
 
   return (
     <main className={`flex min-h-screen flex-col items-center justify-around ${twColorClasses.BG_PRIMARY} p-24`}>
-      <a href="/projects" className={`absolute left-24 top-32 text-lg cursor-pointer ${twColorClasses.TEXT_TERTIARY}`}>
+      <a href="/projects" className={`absolute left-24 top-28 cursor-pointer text-lg ${twColorClasses.TEXT_TERTIARY}`}>
         &lt; Back
       </a>
       {project?.detailImages && <Carousel images={project?.detailImages} />}
       {!project?.detailImages?.length && project?.imageSrc && (
         <img src={project.imageSrc} alt="" className="h-52 w-full object-cover dark:bg-gray-500" />
       )}
-      <div className="max-w-screen-lg">
+      <div className="mt-10 max-w-screen-md">
         <h2 className={`${twColorClasses.TEXT_TERTIARY} text-2xl font-bold`}>{project?.title}</h2>
         <p className={`mb-4 ${twColorClasses.TEXT_SECONDARY}`}>{project?.company}</p>
-        <p className={`${twColorClasses.TEXT_PRIMARY} leading-loose`}>{project?.description}</p>
+        <p className={`${twColorClasses.TEXT_PRIMARY} whitespace-pre-line leading-loose`}>{project?.description}</p>
       </div>
     </main>
   );
--- src/constants/Projects.ts
+++ src/constants/Projects.ts
@@ -17,9 +17,7 @@ const Projects: { [key: string]: IProject } = {
   DIGIDECK_COMPONENTS: {
     id: "digideck-components",
     title: "DIGIDECK Components",
-    description: `The goal of this project was to architect a way for any developer internally or externally to create a DIGIDECK Presentation Component, which is a modular part of the presentation content such as: images, videos, icons, arrows, and anything else you wish to create. During this project I saw to
-  
-creating an API called digideckCORE that allowed developers to interact more easily with our presentation to retrieve metadata, send events, etc. I also created a CLI tool to bootstrap a new Component Repo with the architecture setup similar to the create-react-app tool. Components are Custom Web Components that developers can create custom forms to allow DIGIDECK clients to directly interact with the Component’s settings and for the component to dynamically render content.
+    description: `The goal of this project was to architect a way for any developer internally or externally to create a DIGIDECK Presentation Component, which is a modular part of the presentation content such as: images, videos, icons, arrows, and anything else you wish to create. \n\nDuring this project I saw to creating an API called digideckCORE that allowed developers to interact more easily with our presentation to retrieve metadata, send events, etc. I also created a CLI tool to bootstrap a new Component Repo with the architecture setup similar to the create-react-app tool. Components are Custom Web Components that developers can create custom forms to allow DIGIDECK clients to directly interact with the Component's settings and for the component to dynamically render content.
 `,
     shortDescription: "Modular design elements for the DIGIDECK presentation platform.",
     imageSrc: "/images/COMPONENT_STORE.png",
@@ -53,7 +51,7 @@ creating an API called digideckCORE that allowed developers to interact more eas
     title: "DIGIDECK Design Editor",
     shortDescription:
       "A WYSIWYG Photoshop/Canva like editor that allows for drag to resize, move, and rotate elements.",
-    description: `Using Typescript, Webpack, and NodeJS, I architected and developed a Photoshop/Canva like experience that allowed users to click and drag to edit the position and size of elements on their presentations. Previously, users had to know CSS to style their presentations. I chose Vanilla Typescript because of the amount of complex math and data being calculated from mouse dragging, Typescript allowed much better self documentation and error handling allowing me to work more eciently. I had the challenge of developing an experience that would allow the clients to create responsive designs that would respond to window resizes. Using SOLID/OOPS principles, I created a maintainable and extendable repository that could resize, move and even rotate elements with addition to grid snapping, relative snapping, anchoring to dierent sides, percentage units, and multiple media queries.`,
+    description: `Using Typescript, Webpack, and NodeJS, I architected and developed a Photoshop/Canva like experience that allowed users to click and drag to edit the position and size of elements on their presentations. Previously, users had to know CSS to style their presentations. \n\nI chose Vanilla Typescript because of the amount of complex math and data being calculated from mouse dragging, Typescript allowed much better self documentation and error handling allowing me to work more eciently. I had the challenge of developing an experience that would allow the clients to create responsive designs that would respond to window resizes. \n\nUsing SOLID/OOPS principles, I created a maintainable and extendable repository that could resize, move and even rotate elements with addition to grid snapping, relative snapping, anchoring to dierent sides, percentage units, and multiple media queries.`,
     linkUrl: "https://www.thedigideck.com/design/",
     skillsUsed: ["Typescript", "NodeJS", "Webpack", "SOLID/OOPS", "CI/CD", "Git"],
     company: "Sportsdigita",
@@ -76,7 +74,7 @@ creating an API called digideckCORE that allowed developers to interact more eas
     company: "SideShift",
     linkUrl: "https://www.sideshiftjobs.com/",
     skillsUsed: ["Typescript", "React Native", "Expo", "PostgresSQL", "Supabase", "Git", "Xcode", "Adobe XD"],
-    description: `SideShift is a startup app that connects college students to local hiring businesses. I was brought on as a consultant to oversee the architecture of a React Native App that would work on Web, Iphone and Andriod, using Supabase Postgres DB as a backend. Using the Expo framework, I worked with junior devs to develop a social media like job board that allowed students to scroll through job postings and employers to create job listings as well as create profiles, message students, and send applications.`,
+    description: `SideShift is a startup app that connects college students to local hiring businesses. I was brought on as a consultant to oversee the architecture of a React Native App that would work on Web, Iphone and Andriod, using Supabase Postgres DB as a backend. \n\nUsing the Expo framework, I worked with junior devs to develop a social media like job board that allowed students to scroll through job postings and employers to create job listings as well as create profiles, message students, and send applications.`,
     date: new Date("2024-4-1"),
     imageSrc: "/images/projectPhone.webp",
     detailImages: [{ src: "/images/projectPhone.webp" }],
