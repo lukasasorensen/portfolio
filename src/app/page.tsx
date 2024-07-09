68f4073bea9d53410a6ab5808633f5340a50b34f
Lukas Sorensen <lsorensen@sportsdigita.com>
2024-07-09 17:44:57 -0500

    remove buttons from homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,13 +9,9 @@ export default function Home() {
 
   return (
     <main className={`flex min-h-screen flex-col items-center justify-around ${twColorClasses.BG_PRIMARY} p-24`}>
-      <div className="text-center">
+      <div className="pb-16 text-center">
         <h1 className={`mb-5 text-center text-7xl font-bold ${twColorClasses.TEXT_SECONDARY}`}>Lukas A Sorensen</h1>
         <h2 className={`text-2xl uppercase ${twColorClasses.TEXT_TERTIARY}`}>Full Stack Engineer</h2>
-      </div>
-      <div className="flex w-full max-w-screen-sm justify-evenly">
-        <ThemedButton color="primary" className="text-2xl" text="Resume" onClick={() => router.push("/resume")} />
-        <ThemedButton color="secondary" className="text-2xl" text="Contact" onClick={() => router.push("/contact")} />
       </div>
     </main>
   );
