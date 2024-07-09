bdc4756a7415552124b0478ad4976c0b9fa8094d
Lukas Sorensen <lsorensen@sportsdigita.com>
2024-07-09 17:59:28 -0500

    add buttons back to homepage, fix theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,6 +13,10 @@ export default function Home() {
         <h1 className={`mb-5 text-center text-7xl font-bold ${twColorClasses.TEXT_SECONDARY}`}>Lukas A Sorensen</h1>
         <h2 className={`text-2xl uppercase ${twColorClasses.TEXT_TERTIARY}`}>Full Stack Engineer</h2>
       </div>
+      <div className="flex w-full max-w-screen-sm justify-evenly">
+        <ThemedButton color="primary" className="text-2xl" text="Resume" onClick={() => router.push("/resume")} />
+        <ThemedButton color="secondary" className="text-2xl" text="Contact" onClick={() => router.push("/contact")} />
+      </div>
     </main>
   );
 }
