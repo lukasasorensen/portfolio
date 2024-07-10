137532baad8db4b12dd4fd812149fe6b9e406be2
Lukas Sorensen <lsorensen@sportsdigita.com>
2024-07-09 19:39:28 -0500

    update bg secondary theme, use theme in navbar

--- src/constants/ColorTheme.ts
+++ src/constants/ColorTheme.ts
@@ -49,7 +49,7 @@ export const TailWindColorThemeClasses: ITailWindColorThemeClasses = {
   TEXT_SECONDARY: "text-cyan-800 dark:text-cyan-600",
   TEXT_TERTIARY: "text-violet-800 dark:text-violet-500",
   BG_PRIMARY: "bg-slate-300 dark:bg-slate-700",
-  BG_SECONDARY: "bg-slate-100 dark:bg-stone-800",
+  BG_SECONDARY: "bg-slate-500 dark:bg-gray-800",
   BG_TERTIARY: "bg-violet-600 dark:bg-violet-600",
   BTN_PRIMARY: "bg-violet-600 text-white dark:bg-violet-600 dark:text-white dark:hover:bg-cyan-700",
   BTN_SECONDARY: "bg-cyan-900 text-white dark:bg-cyan-900 dark:text-white dark:hover:bg-violet-500",
