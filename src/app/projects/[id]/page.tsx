fe8b4fa8edb34d4bc701f108b4a726b410b340ac
Lukas Sorensen <lsorensen@sportsdigita.com>
2026-05-07 11:25:51 -0500

    fixes

--- src/app/projects/[id]/page.tsx
+++ src/app/projects/[id]/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 import Image from "next/image";
-import Projects, { IProject } from "@/example-data/Projects";
-import { useState } from "react";
+import Projects from "@/example-data/Projects";
 import { useParams } from "next/navigation";
 import Link from "next/link";
 import Carousel from "@/components/common/Carousel";
@@ -9,20 +8,7 @@ import { TailWindColorThemeClasses as tw } from "@/constants/ColorTheme";
 
 export default function ProjectDetail() {
   const params = useParams<{ id: string }>();
-
-  const [project, setProject] = useState<IProject | null>(null);
-
-  const getProjectById = async (id: string): Promise<IProject | undefined> => {
-    return Object.values(Projects).find((p) => p.id === id);
-  };
-
-  const init = async () => {
-    const getProject = await getProjectById(params.id);
-    if (!getProject) return;
-    setProject(getProject);
-  };
-
-  init();
+  const project = Object.values(Projects).find((p) => p.id === params.id);
 
   return (
     <main className={`flex min-h-screen flex-col items-center justify-around ${tw.BG_PRIMARY} p-8 py-10 md:px-24`}>
